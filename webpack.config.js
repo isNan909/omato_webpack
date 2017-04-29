9640ccfae1d57fbe24a283422a4b7fc20c9b8e9a
Ishan Manandhar <ishan.manandhar@wolfmatrix.com>
2017-04-29 20:52:27 +0545

    style,css,scss loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
+
 module.exports = {
   entry: './src/app.js',
   output: {
     path: __dirname + '/dist',
     filename:  'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {test: /\.scss$/, loaders: ['style-loader','css-loader','sass-loader']}
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin(),
