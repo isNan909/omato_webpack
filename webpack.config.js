f1666ba8cbef20d53853bf8a28a0b3f79d6bf52d
Ishan Manandhar <ishan.manandhar@wolfmatrix.com>
2017-04-29 22:41:38 +0545

    multiple templates and rimraf build for production

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,13 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  entry: './src/app.js',
+  entry: {
+    app: './src/app.js',
+    contact: './src/contact.js'
+  },
   output: {
     path: __dirname + '/dist',
-    filename:  'app.bundle.js'
+    filename:  '[name].bundle.js'
   },
   module: {
     rules: [
@@ -32,7 +35,18 @@ module.exports = {
         collapseWhitespace: true
       },
       hash : true,
+      excludeChunks: ['contact'],
       template: './src/index.ejs', // Load a custom template (ejs by default see the FAQ for details)
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Contact Page',
+      minify: {
+        collapseWhitespace: true
+      },
+      hash : true,
+      chunks: ['contact'],
+      filename: 'contact.html',
+      template: './src/contact.ejs', // Load a custom template (ejs by default see the FAQ for details)
     }),
     new ExtractTextPlugin ("app.css")
   ]
